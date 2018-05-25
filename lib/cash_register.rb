47ba6897eb1eb32572cb1b69d00ee10db1b28071
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-25 16:13:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
 
+class CashRegister
+  attr_accessor = :total
+  attr_reader = :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def cash_register_with_discount
+    @discount
+  end
+  
+end 
