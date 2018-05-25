50b7920edebf00006154baaba542406f2ed513ae
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-25 16:56:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,4 +10,9 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price * quantity
   end
+  
+  def apply_discount
+    @total = @total * (1 - discount/100)
+  end
+  
 end 
